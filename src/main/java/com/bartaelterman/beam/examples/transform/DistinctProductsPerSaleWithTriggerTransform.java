09bc17267ba7e7c482e89953269435083b0fb35e
bartaelterman <bart.aelterman@gmail.com>
2018-08-23 23:22:07 +0200

    separate test for with triggering and without

---
+++ src/main/java/com/bartaelterman/beam/examples/transform/DistinctProductsPerSaleWithTriggerTransform.java
@@ -0,0 +1,107 @@
+package com.bartaelterman.beam.examples.transform;
+
+import org.apache.beam.sdk.transforms.Combine;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.windowing.*;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.joda.time.Duration;
+
+import java.io.Serializable;
+import java.util.*;
+
+
+public class DistinctProductsPerSaleWithTriggerTransform extends PTransform<PCollection<String>, PCollection<KV<String, String>>> {
+
+    private static final int WINDOWGAPDURATION = 600;
+    private static final int TRIGGERINGTIME = 30;
+    static final Logger LOG = LoggerFactory.getLogger(DistinctProductsPerSaleWithTriggerTransform.class);
+
+    // Parse the sale ID and product ID from the string
+    static class SaleIdAsKeyFn extends DoFn<String, KV<String, String>> {
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            String productString = c.element();
+            LOG.info("Processing " + productString);
+            String[] splits = productString.split(" ");
+            if (splits.length == 2) {
+                String saleID = splits[0];
+                String productID = splits[1];
+                c.output(KV.of(saleID, productID));
+            }
+        }
+    }
+
+    // A simple accumulator. Just keep a set of the product IDs
+    public static class Accum implements Serializable {
+        HashSet<String> products = new HashSet<>();
+
+        public void set(String productID) {
+            if (!products.contains(productID)) {
+                products.add(productID);
+            }
+        }
+
+        public Collection<String> getProducts () {
+            return products;
+        }
+    }
+
+    // Custom Combine function to get the unique product IDs per sale
+    public class DistinctProductsFn extends Combine.CombineFn<String, Accum, String> {
+        @Override
+        public Accum createAccumulator() {
+            return new Accum();
+        }
+
+        @Override
+        public Accum addInput(Accum accum, String productID) {
+            accum.set(productID);
+            return accum;
+        }
+
+        @Override
+        public Accum mergeAccumulators(Iterable<Accum> accumulators) {
+            Accum merged = createAccumulator();
+            for (Accum accum: accumulators) {
+                for (String productID: accum.getProducts()) {
+                    merged.set(productID);
+                }
+            }
+            return merged;
+        }
+
+        @Override
+        public String extractOutput(Accum accumulator) {
+            ArrayList<String> l = new ArrayList<>(accumulator.getProducts());
+            Collections.sort(l);
+            return String.join(",", l);
+        }
+    }
+
+    @Override
+    public PCollection<KV<String, String>> expand(PCollection<String> input) {
+        return input
+
+                // Set the sale ID as key and the product ID as the value
+                .apply("SaleIdAsKey", ParDo.of(new SaleIdAsKeyFn()))
+
+                // Set session windows on the sale ID, because we will want to aggregate all
+                // products per sale
+                .apply(Window.<KV<String, String>>into(Sessions.withGapDuration(Duration.standardSeconds(WINDOWGAPDURATION)))
+                        .triggering(Repeatedly.forever(AfterProcessingTime
+                                .pastFirstElementInPane()
+                                .plusDelayOf(Duration.standardSeconds(TRIGGERINGTIME))
+                        ).orFinally(AfterWatermark.pastEndOfWindow()))
+                        .accumulatingFiredPanes()
+                        .withAllowedLateness(Duration.ZERO))
+                // Apply the custom DistinctProductsFunc. A Combine function that will
+                // aggregate all products per window, and return the distinct product IDs
+                // per sale.
+                .apply("Aggregate",  Combine.perKey(new DistinctProductsFn()));
+    }
+}
--- src/test/java/com/bartaelterman/beam/examples/transform/TestDistinctProductsPerSale.java
+++ src/test/java/com/bartaelterman/beam/examples/transform/TestDistinctProductsPerSale.java
@@ -8,15 +8,9 @@ import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.testing.TestPipelineOptions;
 import org.apache.beam.sdk.testing.TestStream;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.windowing.*;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.TimestampedValue;
-import org.apache.beam.sdk.values.TypeDescriptors;
 import org.joda.time.Duration;
 import org.joda.time.Instant;
 import org.junit.Rule;
@@ -28,7 +22,6 @@ import org.junit.runners.JUnit4;
 public class TestDistinctProductsPerSale {
 
     private static final Instant NOW = new Instant(0);
-    private static final Instant SEC_1_DURATION = NOW.plus(Duration.standardSeconds(1));
     private TestPipelineOptions options = PipelineOptionsFactory.fromArgs(
             "--project=project",
             "--tempLocation=gs://bucket/beam")
@@ -38,43 +31,7 @@ public class TestDistinctProductsPerSale {
     public final transient TestPipeline pipeline = TestPipeline.fromOptions(options);
 
     @Test
-    public void should_emit_early_results_after_receiving_6_events() {
-        Coder<String> utfCoder = StringUtf8Coder.of();
-        TestStream<String> onTimeLetters = TestStream.create(utfCoder)
-                .addElements(
-                        TimestampedValue.of("a", SEC_1_DURATION),
-                        TimestampedValue.of("a", SEC_1_DURATION)
-                )
-                .addElements(
-                        TimestampedValue.of("a", SEC_1_DURATION),
-                        TimestampedValue.of("a", SEC_1_DURATION),
-                        TimestampedValue.of("a", SEC_1_DURATION),
-                        TimestampedValue.of("a", SEC_1_DURATION)
-                )
-                .addElements(TimestampedValue.of("a", SEC_1_DURATION))
-                .advanceWatermarkToInfinity();
-        Duration windowDuration = Duration.standardSeconds(5);
-        Window<String> window = Window.<String>into(FixedWindows.of(Duration.standardSeconds(5)))
-                .triggering(AfterPane.elementCountAtLeast(3))
-                .withAllowedLateness(windowDuration, Window.ClosingBehavior.FIRE_ALWAYS)
-                .accumulatingFiredPanes();
-
-        PCollection<String> results = applyCounter(pipeline, window, onTimeLetters);
-
-        // doc: "Triggers allow Beam to emit early results, before all the data in a given window has arrived.
-        // For example, emitting after a certain amount of time elapses, or after a certain number of elements arrives."
-        // Here we want to emit the results after the window receives at least 3 items. In this case we compute the
-        // result for 2 .addElements operations (2 + 4 items). The last .addElements is ignored
-        // See the next test to discover what happens if we add only 1 event in .addElements method
-        IntervalWindow window1 = new IntervalWindow(NOW, NOW.plus(Duration.standardSeconds(5)));
-        PAssert.that(results).inWindow(window1).containsInAnyOrder("a=6");
-        PAssert.that(results).inFinalPane(window1).containsInAnyOrder("a=6");
-
-        pipeline.run().waitUntilFinish();
-    }
-
-    @Test
-    public void EmittedProducts() {
+    public void TestSessionWindow() {
         Coder<String> utfCoder = StringUtf8Coder.of();
         TestStream<String> onTimeProducts = TestStream.create(utfCoder).addElements(
                 TimestampedValue.of("campaign1 product1", new Instant(0)),
@@ -82,22 +39,9 @@ public class TestDistinctProductsPerSale {
                 TimestampedValue.of("campaign1 product3", new Instant(0))
         )
                 .advanceWatermarkTo(new Instant(700)) // watermark passes trigger time
-//                .advanceProcessingTime(new Duration(40))
         .addElements(
                 TimestampedValue.of("campaign1 product9", new Instant(710))
         )
-//                .advanceWatermarkTo(new Instant(90)) // watermark passes trigger time
-////                .advanceProcessingTime(new Duration(50))
-//        .addElements(
-//            TimestampedValue.of("campaign1 product2", new Instant(95)),
-//            TimestampedValue.of("campaign1 product4", new Instant(100))
-//        )
-//                .advanceWatermarkTo(new Instant(710))
-////                .advanceProcessingTime(new Duration(650))
-//                .addElements(
-//                        TimestampedValue.of("campaign1 product9", new Instant(715))
-//                )
-//                .advanceProcessingTime(new Duration(20))
         .advanceWatermarkToInfinity();
 
         PCollection<KV<String, String>> results = applyDistinctProductsTransform(pipeline, onTimeProducts);
@@ -105,20 +49,45 @@ public class TestDistinctProductsPerSale {
         PAssert.that(results).containsInAnyOrder(
                 KV.of("campaign1", "product1,product2,product3"),
                 KV.of("campaign1", "product9")
-//                KV.of("campaign2", "productxyz")
         );
         pipeline.run().waitUntilFinish();
     }
 
-    private static PCollection<String> applyCounter(Pipeline pipeline, Window<String> window,
-                                                    TestStream<String> inputCollection) {
-        return pipeline.apply(inputCollection)
-                .apply(window)
-                .apply(MapElements.into(TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.integers()))
-                        .via((String letter) -> KV.of(letter, 1)))
-                .apply(Count.perKey())
-                .apply(MapElements.into(TypeDescriptors.strings()).via((KV<String, Long> pair) ->
-                        pair.getKey() + "=" + pair.getValue()));
+    @Test
+    public void TestSessionsWithTriggers() {
+        Coder<String> utfCoder = StringUtf8Coder.of();
+        TestStream<String> onTimeProducts = TestStream.create(utfCoder).addElements(
+                TimestampedValue.of("campaign1 product1", new Instant(0)),
+                TimestampedValue.of("campaign1 product2", new Instant(0)),
+                TimestampedValue.of("campaign1 product3", new Instant(0))
+        )
+                .advanceWatermarkTo(new Instant(700)) // watermark passes trigger time
+                .advanceProcessingTime(new Duration(40))
+        .addElements(
+                TimestampedValue.of("campaign1 product9", new Instant(710))
+        )
+                .advanceWatermarkTo(new Instant(90)) // watermark passes trigger time
+                .advanceProcessingTime(new Duration(50))
+        .addElements(
+            TimestampedValue.of("campaign1 product2", new Instant(95)),
+            TimestampedValue.of("campaign1 product4", new Instant(100))
+        )
+                .advanceWatermarkTo(new Instant(710))
+                .advanceProcessingTime(new Duration(650))
+                .addElements(
+                        TimestampedValue.of("campaign1 product9", new Instant(715))
+                )
+                .advanceProcessingTime(new Duration(20))
+        .advanceWatermarkToInfinity();
+
+        PCollection<KV<String, String>> results = applyDistinctProductsTransform(pipeline, onTimeProducts);
+
+        PAssert.that(results).containsInAnyOrder(
+                KV.of("campaign1", "product1,product2,product3"),
+                KV.of("campaign1", "product9"),
+                KV.of("campaign2", "productxyz")
+        );
+        pipeline.run().waitUntilFinish();
     }
 
     private static PCollection<KV<String, String>> applyDistinctProductsTransform(Pipeline pipeline,
